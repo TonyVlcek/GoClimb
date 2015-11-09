1429f86a5494e8de73619b08e9ffa7561da579e4
Tomáš Blatný <blatny.tomas@seznam.cz>
2015-11-09 18:58:28 +0100

    Some changes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,11 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/Framework.php'; // FIXME FIXME FIXME
 
+$localBootstrap = __DIR__ . '/bootstrap.local.php';
+if (file_exists($localBootstrap)) {
+	require $localBootstrap;
+}
+
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
