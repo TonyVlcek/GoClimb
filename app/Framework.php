7987eb6e7e897fb4f31b48ebcc8065f3b806871e
Tomáš Blatný <blatny.tomas@seznam.cz>
2015-11-12 23:32:22 +0100

    Converted indents to tabs

--- app/Framework.php
+++ app/Framework.php
@@ -8,7 +8,7 @@ namespace Nette;
 class Framework
 {
 	const NAME = 'Nette Framework';
-    const VERSION = '2.3.7';
-    const VERSION_ID = 20307;
-    const REVISION = 'released on 2015-10-14';
+	const VERSION = '2.3.7';
+	const VERSION_ID = 20307;
+	const REVISION = 'released on 2015-10-14';
 }
