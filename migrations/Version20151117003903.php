f74cde9ed88bd0b65199cba27f0b5b8980374901
Tomáš Blatný <blatny.tomas@seznam.cz>
2015-11-17 00:39:22 +0100

    Wall: Updated migration

--- migrations/Version20151117003903.php
+++ migrations/Version20151117003903.php
@@ -9,7 +9,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20151117001636 extends AbstractMigration
+class Version20151117003903 extends AbstractMigration
 {
 	/**
 	 * @param Schema $schema
@@ -19,8 +19,6 @@ class Version20151117001636 extends AbstractMigration
 		// this up() migration is auto-generated, please modify it to your needs
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-		$this->addSql('ALTER TABLE role ADD CONSTRAINT FK_57698A6A727ACA70 FOREIGN KEY (parent_id) REFERENCES role (id)');
-		$this->addSql('ALTER TABLE role ADD CONSTRAINT FK_57698A6AC33923F1 FOREIGN KEY (wall_id) REFERENCES wall (id)');
 		$this->addSql('ALTER TABLE wall ADD name VARCHAR(255) NOT NULL');
 		$this->addSql('CREATE UNIQUE INDEX UNIQ_13F5EFF65E237E06 ON wall (name)');
 	}
@@ -33,8 +31,6 @@ class Version20151117001636 extends AbstractMigration
 		// this down() migration is auto-generated, please modify it to your needs
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-		$this->addSql('ALTER TABLE role DROP FOREIGN KEY FK_57698A6A727ACA70');
-		$this->addSql('ALTER TABLE role DROP FOREIGN KEY FK_57698A6AC33923F1');
 		$this->addSql('DROP INDEX UNIQ_13F5EFF65E237E06 ON wall');
 		$this->addSql('ALTER TABLE wall DROP name');
 	}
