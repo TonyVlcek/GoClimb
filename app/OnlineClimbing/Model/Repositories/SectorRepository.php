8975f9e9f162fa8a79f03079b0dd8ff59127c907
TonyVlcek <vlcek.a@email.cz>
2015-11-17 18:51:53 +0100

    Sector: Added Sector Entity and Repository

---
+++ app/OnlineClimbing/Model/Repositories/SectorRepository.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Tony Vlček
+ */
+
+namespace OnlineClimbing\Model\Repositories;
+
+use OnlineClimbing\Model\Entities\Sector;
+use OnlineClimbing\Model\Entities\Wall;
+
+
+class SectorRepository extends BaseRepository
+{
+
+	/**
+	 * @param Wall $wall
+	 * @param string $name
+	 * @return Sector|NULL
+	 */
+	public function getByName(Wall $wall, $name)
+	{
+		return $this->getDoctrineRepository()->findOneBy(["wall"=> $wall,"name" => $name]);
+	}
+
+
+	/**
+	 * @param Wall $wall
+	 * @return Sector[]
+	 */
+	public function getByWall(Wall $wall)
+	{
+		return $this->getDoctrineRepository()->findBy(["wall" => $wall]);
+	}
+}
