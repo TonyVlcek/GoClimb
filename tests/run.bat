39e0c58734f947bc23a01046ef9fc9e84a579b20
TonyVlcek <vlcek.a@email.cz>
2015-12-10 21:21:24 +0100

    Tests: Refactored php-XX.ini files

--- tests/run.bat
+++ tests/run.bat
@@ -1,1 +1,1 @@
-./vendor/bin/tester ./tests/%1 -c ./tests/php-win.ini --setup ./tests/setup.php -p php-cgi -s
+./vendor/bin/tester ./tests/%1 -c ./tests/php-local.ini --setup ./tests/setup.php -p php-cgi -s
--- tests/run.sh
+++ tests/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./vendor/bin/tester ./tests/$1 -c ./tests/php-unix.ini --setup ./tests/setup.php -p php-cgi -s
+./vendor/bin/tester ./tests/$1 -c ./tests/php-local.ini --setup ./tests/setup.php -p php-cgi -s
