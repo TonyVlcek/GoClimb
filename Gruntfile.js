929eb85c43d172090ba9e8d1f08f33fbd76a945d
Tomáš Blatný <blatny.tomas@seznam.cz>
2016-02-13 14:44:54 +0100

    [GC-48] Gruntfile.js: refactored to allow building parts of application alone

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,125 +1,64 @@
 module.exports = function (grunt) {
-	var banner = '/*! <%= pkg.name %> v<%= pkg.version %> built <%= grunt.template.today("yyyy-mm-dd") %> */\n';
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
-		/** SASS */
-		sass: {
-			app: {
-				options: {
-					sourcemap: 'none',
-					style: 'compressed'
-				},
-				files: {
-					'www/assets/build/app.css': [
-						'www/css/app/**/*.scss'
-					]
-				}
-			}
-		},
-
-		/** TypeScript */
-		ts: {
-			app: {
-				files: {
-					'www/assets/build/app.js': 'www/js/**/*.ts'
-				}
-			}
-		},
-
-		/** CSS and JS libs */
-		bower_concat: {
-			options: {
-				separator: ';\n'
-			},
-			app: {
-				dest: 'www/assets/build/libs.js',
-				cssDest: 'www/assets/build/libs.css',
-				mainFiles: {
-					/*bootstrap: [
-						'dist/css/bootstrap.min.css',
-						'dist/css/bootstrap-theme.min.css',
-						'dist/css/bootstrap.min.js'
-					],*/
-					'jquery': [
-						'dist/jquery.min.js'
-					],
-					'angular': [
-						'angular.min.js'
-					],
-					'angular-animate': [
-						'angular-animate.min.js'
-					],
-					'angular-ui-route': [
-						'release/angular-ui-router.min.js'
-					],
-					'fastclick': [
-						'lib/fastclick.js'
-					],
-					/*,
-						'hammerjs': [
-						'hammer.min.js'
-					]*/
-					'tether': [
-						'tether.min.js',
-						'css/tether.css',
-						'css/tether-theme-basic.css',
-						'css/tether-theme-arrows.css',
-						'css/tether-theme-arrows-dark.css'
-					],
-					'viewport-units-buggyfill': [
-						'viewport-units-buggyfill.js',
-						'viewport-units-buggyfill.hacks.js'
-					],
-					'foundation-apps': [
-						'dist/css/foundation-apps.min.css',
-						'dist/js/foundation-apps.min.js',
-						'dist/js/foundation-apps-templates.min.js'
-					]
-				}
-			}
-		},
-
-		/** Concat CSS files */
-		concat: {
-			options: {
-				banner: banner,
-				stripBanners: true
-			},
-			app: {
-				src: [
-					'www/assets/build/libs.css',
-					'www/assets/build/app.css'
-				],
-				dest: 'www/assets/app.css'
-			}
-		},
-
-		/** Minification */
-		uglify: {
-			options: {
-				banner: banner
-			},
-			app: {
-				src: [
-					'www/assets/build/libs.js',
-					'www/assets/build/app.js'
-				],
-				dest: 'www/assets/app.js'
-			}
-		}
+		sass: grunt.file.readJSON('app/config/build/sass.json'),
+		ts: grunt.file.readJSON('app/config/build/ts.json'),
+		concat: grunt.file.readJSON('app/config/build/concat.json'),
+		uglify: grunt.file.readJSON('app/config/build/uglify.json'),
+		ngtemplates: grunt.file.readJSON('app/config/build/ngtemplates.json'),
+		clean: [
+			'!www/assets/build/.gitkeep',
+			'www/assets/build/*'
+		]
 	});
 
-	grunt.loadNpmTasks('grunt-bower-concat');
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-ts');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-angular-templates');
 
+	/** SASS */
+	grunt.registerTask('sass:app', ['sass:admin', 'sass:auth', 'sass:backend', 'sass:frontend']);
+	grunt.registerTask('sass:app-dev', ['sass:admin-dev', 'sass:auth-dev', 'sass:backend-dev', 'sass:frontend-dev']);
 
-	grunt.registerTask('build:css', ['bower_concat:app', 'sass:app', 'concat:app']);
-	grunt.registerTask('build:js', ['bower_concat:app', 'ts:app', 'uglify:app']);
+	/** TS */
+	grunt.registerTask('ts:admin-dev', ['ts:admin']);
+	grunt.registerTask('ts:auth-dev', ['ts:auth']);
+	grunt.registerTask('ts:backend-dev', ['ts:backend']);
+	grunt.registerTask('ts:frontend-dev', ['ts:frontend']);
 
-	grunt.registerTask('build', ['bower_concat:app', 'sass:app', 'concat:app', 'ts:app', 'uglify:app']);
+	grunt.registerTask('ts:app', ['ts:admin', 'ts:auth', 'ts:backend', 'ts:frontend']);
+	grunt.registerTask('ts:app-dev', ['ts:admin-dev', 'ts:auth-dev', 'ts:backend-dev', 'ts:frontend-dev']);
+
+	/** ANGULAR TEMPLATES */
+	grunt.registerTask('ngtemplates:app', ['ngtemplates:admin', 'ngtemplates:auth', 'ngtemplates:backend', 'ngtemplates:frontend']);
+	grunt.registerTask('ngtemplates:app-dev', ['ngtemplates:admin-dev', 'ngtemplates:auth-dev', 'ngtemplates:backend-dev', 'ngtemplates:frontend-dev']);
+
+	/** ONE FILE */
+	grunt.registerTask('concat:app', ['concat:admin', 'concat:auth', 'concat:backend', 'concat:frontend']);
+	grunt.registerTask('concat:app-dev', ['concat:admin-dev', 'concat:auth-dev', 'concat:backend-dev', 'concat:frontend-dev']);
+
+	grunt.registerTask('uglify:app', ['uglify:admin', 'uglify:auth', 'uglify:backend', 'uglify:frontend']);
+	grunt.registerTask('uglify:app-dev', ['uglify:admin-dev', 'uglify:auth-dev', 'uglify:backend-dev', 'uglify:frontend-dev']);
+
+	/** SHORTCUTS  */
+	grunt.registerTask('build:admin', ['sass:admin', 'ts:admin', 'concat:admin', 'uglify:admin', 'ngtemplates:admin', 'clean']);
+	grunt.registerTask('build:admin-dev', ['sass:admin-dev', 'ts:admin-dev', 'concat:admin-dev', 'uglify:admin-dev', 'ngtemplates:admin-dev', 'clean']);
+
+	grunt.registerTask('build:auth', ['sass:auth', 'ts:auth', 'concat:auth', 'uglify:auth', 'ngtemplates:auth', 'clean']);
+	grunt.registerTask('build:auth-dev', ['sass:auth-dev', 'ts:auth-dev', 'concat:auth-dev', 'uglify:auth-dev', 'ngtemplates:auth-dev', 'clean']);
+
+	grunt.registerTask('build:backend', ['sass:backend', 'ts:backend', 'concat:backend', 'uglify:backend', 'ngtemplates:backend', 'clean']);
+	grunt.registerTask('build:backend-dev', ['sass:backend-dev', 'ts:backend-dev', 'concat:backend-dev', 'uglify:backend-dev', 'ngtemplates:backend-dev', 'clean']);
+
+	grunt.registerTask('build:frontend', ['sass:frontend', 'ts:frontend', 'concat:frontend', 'uglify:frontend', 'ngtemplates:frontend', 'clean']);
+	grunt.registerTask('build:frontend-dev', ['sass:frontend-dev', 'ts:frontend-dev', 'concat:frontend-dev', 'uglify:frontend-dev', 'ngtemplates:frontend-dev', 'clean']);
+
+	grunt.registerTask('build:app', ['sass:app', 'ts:app', 'concat:app', 'uglify:app', 'ngtemplates:app', 'clean']);
+	grunt.registerTask('build:app-dev', ['sass:app-dev', 'ts:app-dev', 'concat:app-dev', 'uglify:app-dev', 'ngtemplates:app-dev', 'clean']);
+
+	grunt.registerTask('default', ['build:app-dev']);
+
 };
