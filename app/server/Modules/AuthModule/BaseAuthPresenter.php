329b2d50daf8481d0b15529ee97c8b3c49b3b81e
TonyVlcek <vlcek.a@email.cz>
2016-02-27 12:29:49 +0100

    BackendModule: added Auth module integration

--- app/server/Modules/AuthModule/BaseAuthPresenter.php
+++ app/server/Modules/AuthModule/BaseAuthPresenter.php
@@ -6,6 +6,7 @@
 namespace GoClimb\Modules\AuthModule;
 
 use GoClimb\Modules\BasePresenter;
+use GoClimb\NotImplementedException;
 use Nette\Application\Request;
 
 
@@ -15,6 +16,15 @@ abstract class BaseAuthPresenter extends BasePresenter
 	protected function init(Request $request)
 	{
 		$this->applicationPartsManager->setAsAuth();
+	}
+
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function getApplicationToken()
+	{
+		throw new NotImplementedException;
 	}
 
 }
