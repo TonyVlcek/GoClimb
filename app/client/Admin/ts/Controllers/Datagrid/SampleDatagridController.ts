0fe37735be2e2462aac951112bc6b891b636e7f7
TonyVlcek <vlcek.a@email.cz>
2016-07-08 11:07:25 +0200

    Sample datagrid using smart table

---
+++ app/client/Admin/ts/Controllers/Datagrid/SampleDatagridController.ts
@@ -0,0 +1,52 @@
+namespace GoClimb.Admin.Controllers.Datagrid
+{
+
+	var goClimb = angular.module('GoClimb');
+
+	goClimb.controller('sampleDatagrid', ['$scope', function($scope) {
+		var firstnames = ['Laurent', 'Blandine', 'Olivier', 'Max'];
+		var lastnames = ['Renard', 'Faivre', 'Frere', 'Eponge'];
+		var dates = ['1987-05-21', '1987-04-25', '1955-08-27', '1966-06-06'];
+		var id = 1;
+
+		function generateRandomItem(id) {
+
+			var firstname = firstnames[Math.floor(Math.random() * 3)];
+			var lastname = lastnames[Math.floor(Math.random() * 3)];
+			var birthdate = dates[Math.floor(Math.random() * 3)];
+			var balance = Math.floor(Math.random() * 2000);
+
+			return {
+				id: id,
+				firstName: firstname,
+				lastName: lastname,
+				birthDate: new Date(birthdate),
+				balance: balance
+			}
+		}
+
+		$scope.rowCollection = [
+			{firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+			{firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+			{firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+		];
+
+		for (id; id < 5; id++) {
+			$scope.rowCollection.push(generateRandomItem(id));
+		}
+
+		//add to the real data holder
+		$scope.addRandomItem = function addRandomItem() {
+			$scope.rowCollection.push(generateRandomItem(id));
+			id++;
+		};
+
+		//remove to the real data holder
+		$scope.removeItem = function removeItem(row) {
+			var index = $scope.rowCollection.indexOf(row);
+			if (index !== -1) {
+				$scope.rowCollection.splice(index, 1);
+			}
+		}
+	}]);
+}
