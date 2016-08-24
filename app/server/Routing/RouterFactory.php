fa1f77f98f8cddd9368987b1cacf08113bb49ab6
Tomáš Blatný <blatny.tomas@seznam.cz>
2016-08-24 14:59:33 +0200

    Routing: reverted most changes

--- app/server/Routing/RouterFactory.php
+++ app/server/Routing/RouterFactory.php
@@ -93,14 +93,7 @@ class RouterFactory
 			'wall' => $filter,
 			'locale' => $wallLanguage->getLanguage()->getShortcut(),
 		]);
-		$router[] = new Route('//' . $this->createRouteUrl($wallLanguage->getUrl(), 'admin') . '/api/v1/<presenter>/<action>[/<id>]', [
-			'module' => 'Wall:Rest:V1',
-			'presenter' => 'Dashboard',
-			'action' => 'default',
-			'wall' => $filter,
-			'locale' => $wallLanguage->getLanguage()->getShortcut(),
-		]);
-		$router[] = new Route('//' . $this->createRouteUrl($wallLanguage->getUrl(), 'admin') . '/[<path .*>]', [
+		$router[] = new Route('//' . $this->createRouteUrl($wallLanguage->getUrl()) . '/admin/[<path .*>]', [
 			'module' => 'Wall:Admin',
 			'presenter' => 'Dashboard',
 			'action' => 'default',
