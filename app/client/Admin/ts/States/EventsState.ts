2ff14046491c8a4738dcd9b88b641a43f1acb809
norik16 <ron.norik@gmail.com>
2016-09-26 23:09:57 +0200

    Admin: fixed permissions in states

--- app/client/Admin/ts/States/EventsState.ts
+++ app/client/Admin/ts/States/EventsState.ts
@@ -2,6 +2,7 @@ namespace GoClimb.Admin.States
 {
 
 	import BaseState = GoClimb.Core.States.BaseState;
+	import Authorizator = GoClimb.Admin.Services.Authorizator;
 
 	export class EventsState extends BaseState
 	{
@@ -9,6 +10,12 @@ namespace GoClimb.Admin.States
 		public url = '/events';
 		public templateUrl = 'app/client/Admin/ts/templates/Events/default.html';
 		public controller = 'EventsController as eventsCtrl';
+
+		public onEnter = ['authorizator', (authorizator: Authorizator) => {
+
+			return authorizator.isAllowed('admin.events');
+
+		}];
 
 	}
 
