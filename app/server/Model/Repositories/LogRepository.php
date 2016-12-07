67652a2313c204fca05153ed343125f5be0af433
Martin Miksik <mamiksik@gmail.com>
2016-12-07 14:00:28 +0100

    Added 1Top DQL command

--- app/server/Model/Repositories/LogRepository.php
+++ app/server/Model/Repositories/LogRepository.php
@@ -6,6 +6,8 @@
 namespace GoClimb\Model\Repositories;
 
 use GoClimb\Model\Entities\Log;
+use GoClimb\Model\Entities\Wall;
+use Nette\Utils\DateTime;
 
 
 class LogRepository extends BaseRepository
@@ -27,5 +29,29 @@ class LogRepository extends BaseRepository
 	public function getAll()
 	{
 		return $this->getDoctrineRepository()->findBy([], ['loggedDate' => 'DESC']);
+	}
+
+	public function getLogsByWalls(Wall ...$walls)
+	{
+		$qb = $this->getEntityManager()->createQueryBuilder();
+		$qb->select('l')
+			->from('GoClimb\Model\Entities\Log', 'l')
+			->leftJoin('l.style', 's')
+			->leftJoin('l.route', 'r')
+			->leftJoin('r.difficulty', 'd')
+			->addSelect('d.points + s.boulderPoints AS sumPoints')
+			->orderBy('sumPoints', 'DESC');
+
+
+		$criteria['loggedDate > '] = new DateTime('-1 year');
+
+		if (is_null($walls)){
+			$criteria['route.line.sector.wall'] = $walls;
+		}
+
+		$qb->whereCriteria($criteria);
+
+		dump($qb->getQuery()->getSQL());
+		return $qb->getQuery()->getResult();
 	}
 }
